963f9b842ab7da32cfe0f65da7c266e915ccbeb9
yanqing4 <yanqing4@staff.sina.com.cn>
2019-03-26 16:08:40 +0800

    调整rg参数

--- yanqing4.sh
+++ yanqing4.sh
@@ -132,7 +132,7 @@ saeget() {
 }
 
 # for fzf need install ripgrep
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!{.git,.svn,node_modules}/*" 2> /dev/null'
+export FZF_DEFAULT_COMMAND='rg --files --smart-case --no-ignore --hidden --follow --glob "!{.git,.svn,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="rg --sort-files --files --null 2> /dev/null | xargs -0 dirname | uniq"
 export HISTIGNORE="y:pwd:ls:cd:ll:clear:history"
